73d900c7cccde0eb65aad1c3860d7a1feb81be39
okutsen <okutsen@softserveinc.com>
2022-04-15 20:43:45 +0300

    refactor context logger

--- internal/log/log.go
+++ internal/log/log.go
@@ -1,64 +1,88 @@
 package log
 
 import (
-	"context"
+	"bytes"
 	"fmt"
 )
 
 // TODO: add metrics
-
-type correlationIDType int
-const (
-	// TODO: generate id in handler, use uuid.NewRandom()
-	requestID correlationIDType = iota
-	clientID
-)
-
-// type LoggerWithFields interface {
-// 	Logger
-// 	WithField(key string, value interface{}) LoggerWithFields
-// }
-
-// Baselogger
 type Logger interface{
-	Fatal(v ...interface{})
-	Fatalf(format string, v ...interface{})
-	Panic(v ...interface{})
-	Panicf(format string, v ...interface{})
-	Print(v ...interface{})
-	Printf(format string, v ...interface{})
-	Info(args ...interface{})
-	Infof(format string, args ...interface{})
-	Warn(args ...interface{})
-	Warnf(format string, args ...interface{})
-	Error(args ...interface{})
-	Errorf(format string, args ...interface{})
+	Fatal(v ...any)
+	Fatalf(format string, v ...any)
+	Panic(v ...any)
+	Panicf(format string, v ...any)
+	Print(v ...any)
+	Printf(format string, v ...any)
+	Info(args ...any)
+	Infof(format string, args ...any)
+	Warn(args ...any)
+	Warnf(format string, args ...any)
+	Error(args ...any)
+	Errorf(format string, args ...any)
 }
 
-func NewWithContext(logger Logger, ctx context.Context) *LoggerWithContext {
-	return &LoggerWithContext{
-		logger: logger,
-		context: ctx,
-	}
-}
+type Fields map[string]any
 
 type LoggerWithContext struct {
 	logger Logger
-	context context.Context
+	fields Fields
+	prefix string
+	sep string
 }
 
-func (lwc *LoggerWithContext) Info(v ...interface{}) {
-	if lwc.context != nil {
-		// TODO: use logrus WithFields or manually construct message
-		var logMessage string
-		if ctxRequestID, ok := lwc.context.Value(requestID).(string); ok {
-			logMessage = fmt.Sprintf("%v		requestID=%s", v, ctxRequestID)
-			// lwc.logger = lwc.logger.WithField("rqId", ctxRqId)
-		}
-		if ctxClientID, ok := lwc.context.Value(clientID).(string); ok {
-			logMessage = fmt.Sprintf("%v		clientID=%s", v, ctxClientID)
-			// lwc.logger = lwc.logger.WithField("sessionId", ctxSessionId)
-		}
-		lwc.logger.Info(logMessage)
+func NewWithFields(logger Logger) *LoggerWithContext {
+	return &LoggerWithContext{
+		logger: logger,
+		prefix: " ",
+		sep: "\t",
 	}
+}
+
+func (lwc *LoggerWithContext) SetPrefix(prefix string) {
+	lwc.prefix = prefix
+}
+
+func (lwc *LoggerWithContext) SetSep(sep string) {
+	lwc.sep = sep
+}
+
+func (lwc *LoggerWithContext) WithFields(fields Fields) *LoggerWithContext {
+	lwc.addFields(fields)
+	return lwc
+}
+
+// to pkg?
+func (lwc *LoggerWithContext) addFields(f Fields) {
+	// concat maps?
+	for k, v := range f {
+		lwc.fields[k] = v
+	}
+}
+
+func (lwc *LoggerWithContext) ComposeMessage(v []any) string {
+	return lwc.composeMessage(fmt.Sprint(v...))
+}
+
+func (lwc *LoggerWithContext) ComposeMessagef(format string, v []any) string {
+	return lwc.composeMessage(fmt.Sprintf(format, v...))
+}
+
+// to pkg?
+func (lwc *LoggerWithContext) composeMessage(initial string) string {
+	var buff bytes.Buffer
+	buff.WriteString(initial)
+	buff.WriteString(lwc.prefix)
+    for key, value := range lwc.fields {
+        fmt.Fprintf(&buff, "%s%s=%v", lwc.sep, key, value)
+    }
+    return buff.String()
+}
+
+// any way not to override every method? 
+func (lwc *LoggerWithContext) Print(v ...any) {
+	lwc.logger.Info(lwc.ComposeMessage(v))
+}
+
+func (lwc *LoggerWithContext) Printf(format string, v ...any) {
+	lwc.logger.Info(lwc.ComposeMessagef(format, v))
 }
