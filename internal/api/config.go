f51587db7bf8b3cfc20b2744c2eb3c2229f26ec3
Dmytro Taranov <dtaran@softserveinc.com>
2022-04-22 14:39:17 +0300

    delete dependency with global config

--- internal/api/config.go
+++ internal/api/config.go
@@ -2,32 +2,13 @@ package api
 
 import (
 	"fmt"
-
-	"github.com/spf13/viper"
 )
 
 type Config struct {
 	Host string
 	Port uint
-	Addr string
-}
-
-func initConfig(configPath string) error {
-	viper.SetConfigFile(configPath)
-	return viper.ReadInConfig()
-}
-
-func NewConfig(configPath string) (*Config, error) {
-	if err := initConfig(configPath); err != nil {
-		return nil, fmt.Errorf("read config: %w", err)
-	}
-	return &Config{
-		Host: viper.GetString("host"),
-		Port: viper.GetUint("port"),
-	}, nil
 }
 
 func (c Config) Address() string {
-	c.Addr = fmt.Sprintf("%s:%v", c.Host, c.Port)
-	return c.Addr
+	return fmt.Sprintf("%s:%d", c.Host, c.Port)
 }
