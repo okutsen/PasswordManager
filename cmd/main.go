66ab9d8308f240c28d454a0738af094d33ce788c
Dmytro Taranov <dtaran@softserveinc.com>
2022-04-22 14:40:18 +0300

    make corrections

--- cmd/main.go
+++ cmd/main.go
@@ -10,14 +10,16 @@ import (
 
 func main() {
 	logger := log.NewLogrusLogger()
-	_, err := config.NewConfig("config/config.yaml")
+	cfg, err := config.NewConfig("config/config.yaml")
 	if err != nil {
 		// TODO: Use default values to configure api
 		logger.Fatalf("initialize config", err)
 	}
-	apiConfig, err := api.NewConfig("config/config.yaml")
 
-	serviceAPI := api.New(apiConfig, logger)
+	serviceAPI := api.New(&api.Config{
+		Host: cfg.Host,
+		Port: cfg.Port,
+	}, logger)
 	err = serviceAPI.Start()
 	// close op objects
 	logger.Fatal(err)
