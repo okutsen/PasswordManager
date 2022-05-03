074d75b287fe2697e623d216b1139ff594aef59f
okutsen <okutsen@softserveinc.com>
2022-05-03 23:41:28 +0300

    add HandlerContext and controller usages

--- internal/controller/controller.go
+++ internal/controller/controller.go
@@ -6,29 +6,32 @@ import (
 	"github.com/okutsen/PasswordManager/internal/log"
 )
 
-type ControllerService struct {
+type Controller struct {
 	// repo Repo
 	log  log.Logger
 }
 
-func New(logger log.Logger) *ControllerService{
-	return &ControllerService{
+func New(logger log.Logger) *Controller{
+	return &Controller{
 		log: logger.WithFields(log.Fields{"service": "Controller"}),
 	}
 }
 
-func (c *ControllerService) GetAllRecords() (string, error) {
+func (c *Controller) GetAllRecords() (string, error) {
 	// queryResult := repo.GetAllRecords()
 	queryResult := "Records:\n0,1,2,3,4,5"
 	return queryResult, nil
 }
 
-func (c *ControllerService) GetRecord(id uint) (string, error) {
+func (c *Controller) GetRecord(id uint64) (string, error) {
 	// TODO: pass uuid
+	// if id < 5 {
+	// 	return "", fmt.Errorf("no record with id %d", id)
+	// }
 	responseBody := fmt.Sprintf("Records:\n%d", id)
 	return responseBody, nil
 }
 
-func (c *ControllerService) CreateRecords() (string, error) {
+func (c *Controller) CreateRecords() (string, error) {
 	return "", nil
 }
