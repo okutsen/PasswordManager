a05e80f595698961739e18bb34ef76a4700706f1
Dmytro Taranov <dtaran@softserveinc.com>
2022-05-08 23:28:15 +0300

    create graceful shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,14 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"net/http"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+
 	"github.com/okutsen/PasswordManager/config"
 	"github.com/okutsen/PasswordManager/internal/api"
 	"github.com/okutsen/PasswordManager/internal/log"
@@ -15,12 +23,30 @@ func main() {
 		logger.Fatalf("initialize config: %v", err)
 	}
 
-	serviceAPI := api.New(&api.Config{
-		Port: cfg.Port,
-	}, logger)
+	serviceAPI := api.New(&api.Config{Port: cfg.Port}, logger)
 
-	err = serviceAPI.Start()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go func() {
+		err = serviceAPI.Start(ctx)
+		if err != nil && !errors.Is(err, http.ErrServerClosed) {
+			logger.Fatalf("start application %v", err)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+
+	<-c
+
+	closeWaitGroup := &sync.WaitGroup{}
+
+	closeWaitGroup.Add(1)
+	err = serviceAPI.Stop(ctx, closeWaitGroup)
 	if err != nil {
-		logger.Fatalf("start application", err)
+		panic(err)
 	}
+
+	closeWaitGroup.Wait()
 }
