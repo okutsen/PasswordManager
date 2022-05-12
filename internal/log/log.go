0af7bef78744ea27fb1e8c8e624ab54ee602b6a7
okutsen <okutsen@softserveinc.com>
2022-05-12 16:56:30 +0300

    add debug methods

--- internal/log/log.go
+++ internal/log/log.go
@@ -9,6 +9,8 @@ type Logger interface {
 	Errorf(format string, args ...any)
 	Fatal(v ...any)
 	Fatalf(format string, v ...any)
+	Debug(v ...any)
+	Debugf(format string, v ...any)
 	WithFields(fields Fields) Logger
 }
 type Fields map[string]any
