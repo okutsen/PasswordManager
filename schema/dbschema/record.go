0b1fc37fbb21a259c71ca4d9bd8aee62b789b999
Dmytro Taranov <dtaran@softserveinc.com>
2022-05-13 17:53:50 +0300

    create user struct, update record struct

--- schema/dbschema/record.go
+++ schema/dbschema/record.go
@@ -1,9 +1,26 @@
 package dbschema
 
-// TODO: more fields (dateCreated, url, description)
+import "time"
+
+type User struct {
+	ID        uint64    `db:"id"`
+	Email     string    `db:"email"`
+	Login     string    `db:"login"`
+	Password  string    `db:"password"`
+	Phone     string    `db:"phone"`
+	CreatedAt time.Time `db:"created_at"`
+	UpdatedAt time.Time `db:"updated_at"`
+}
+
 type Record struct {
-	ID       uint64
-	Name     string
-	Login    string
-	Password string
+	ID          uint64    `db:"id"`
+	Name        string    `db:"name"`
+	Login       string    `db:"login"`
+	Password    string    `db:"password"`
+	URL         string    `db:"url"`
+	Description string    `db:"description"`
+	UpdatedBy   string    `db:"updated_by"`
+	CreatedBy   string    `db:"created_by"`
+	CreatedAt   time.Time `db:"created_at"`
+	UpdatedAt   time.Time `db:"updated_at"`
 }
