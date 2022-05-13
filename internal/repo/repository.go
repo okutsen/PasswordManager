d3b4f060039a0b087b29d52ec5ac7e8cd10744f8
Dmytro Taranov <dtaran@softserveinc.com>
2022-05-13 17:54:15 +0300

    make corrections

--- internal/repo/repository.go
+++ internal/repo/repository.go
@@ -4,7 +4,6 @@ import (
 	"database/sql"
 	"fmt"
 
-	"github.com/okutsen/PasswordManager/config"
 	"github.com/okutsen/PasswordManager/internal/log"
 	"github.com/okutsen/PasswordManager/schema/dbschema"
 )
@@ -13,15 +12,14 @@ type Repo struct {
 	db *sql.DB
 }
 
-func New(cfg config.DBConfig, logger log.Logger) (*Repo, error) {
+func New(cfg *Config, logger log.Logger) (*Repo, error) {
 	logger.Infof("DB is starting")
-	// With struct or const?
 	db, err := sql.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
 		cfg.Host, cfg.Port, cfg.Username, cfg.DBName, cfg.Password, cfg.SSLMode))
 	if err != nil {
 		return nil, fmt.Errorf("failed to open sql connection: %w", err)
 	}
-	// New func or not?
+
 	err = db.Ping()
 	if err != nil {
 		return nil, fmt.Errorf("failed to verify connection to DB: %w", err)
@@ -35,6 +33,6 @@ func (r *Repo) Close() error {
 	return r.db.Close()
 }
 
-func AllRecords() ([]dbschema.Records, error) {
-	return []dbschema.Records{}, nil
+func AllRecords() ([]dbschema.Record, error) {
+	return []dbschema.Record{}, nil
 }
