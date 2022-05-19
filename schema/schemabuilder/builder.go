10b7153ff653cb0b4d4ce28c257f9206bf7c91db
Dmytro Taranov <dtaran@softserveinc.com>
2022-05-19 17:22:01 +0300

    remove pointers, format file

--- schema/schemabuilder/builder.go
+++ schema/schemabuilder/builder.go
@@ -5,36 +5,36 @@ import (
 	"github.com/okutsen/PasswordManager/schema/dbschema"
 )
 
-func BuildRecordAPIFrom(record *dbschema.Record) *apischema.Record {
-	return &apischema.Record{
-		ID: record.ID,
-		Name: record.Name,
-		Login: record.Login,
+func BuildRecordAPIFrom(record *dbschema.Record) apischema.Record {
+	return apischema.Record{
+		ID:       record.ID,
+		Name:     record.Name,
+		Login:    record.Login,
 		Password: record.Password,
 	}
 }
 
-func BuildRecordsAPIFrom(records []*dbschema.Record) []*apischema.Record {
-	recordsAPI := make([]*apischema.Record, len(records))
+func BuildRecordsAPIFrom(records []dbschema.Record) []apischema.Record {
+	recordsAPI := make([]apischema.Record, len(records))
 	for i, v := range records {
-		recordsAPI[i] = BuildRecordAPIFrom(v)
+		recordsAPI[i] = BuildRecordAPIFrom(&v)
 	}
 	return recordsAPI
 }
 
-func BuildRecordFrom(recordAPI *apischema.Record) *dbschema.Record {
-	return &dbschema.Record{
-		ID: recordAPI.ID,
-		Name: recordAPI.Name,
-		Login: recordAPI.Login,
+func BuildRecordFrom(recordAPI *apischema.Record) dbschema.Record {
+	return dbschema.Record{
+		ID:       recordAPI.ID,
+		Name:     recordAPI.Name,
+		Login:    recordAPI.Login,
 		Password: recordAPI.Password,
 	}
 }
 
-func BuildRecordsFrom(recordsAPI []*apischema.Record) []*dbschema.Record {
-	records := make([]*dbschema.Record, len(recordsAPI))
+func BuildRecordsFrom(recordsAPI []apischema.Record) []dbschema.Record {
+	records := make([]dbschema.Record, len(recordsAPI))
 	for i, v := range recordsAPI {
-		records[i] = BuildRecordFrom(v)
+		records[i] = BuildRecordFrom(&v)
 	}
 	return records
 }
