93ea20b787a2d13fea4154de3f7ea1d5b4bc9f5a
Dmytro Taranov <dtaran@softserveinc.com>
2022-05-19 17:23:50 +0300

    change func name, remove pointer

--- internal/controller/controller.go
+++ internal/controller/controller.go
@@ -2,54 +2,28 @@ package controller
 
 import (
 	"github.com/okutsen/PasswordManager/internal/log"
+	"github.com/okutsen/PasswordManager/internal/repo"
 	"github.com/okutsen/PasswordManager/schema/dbschema"
 )
 
 type Controller struct {
-	// repo Repo
-	log log.Logger
+	repo repo.Repo
+	log  log.Logger
 }
 
-func New(logger log.Logger) *Controller {
+func New(logger log.Logger, repo repo.Repo) *Controller {
 	return &Controller{
-		log: logger.WithFields(log.Fields{"service": "Controller"}),
+		log:  logger.WithFields(log.Fields{"service": "Controller"}),
+		repo: repo,
 	}
 }
 
-func (c *Controller) GetAllRecords() ([]*dbschema.Record, error) {
-	// queryResult := repo.GetAllRecords()
-	return []*dbschema.Record{
-		{
-			ID:       1,
-			Name:     "testName",
-			Login:    "testLogin",
-			Password: "testPassword",
-		},
-		{
-			ID:       2,
-			Name:     "testName",
-			Login:    "testLogin",
-			Password: "testPassword",
-		},
-		{
-			ID:       3,
-			Name:     "testName",
-			Login:    "testLogin",
-			Password: "testPassword",
-		},
-		{
-			ID:       4,
-			Name:     "testName",
-			Login:    "testLogin",
-			Password: "testPassword",
-		},
-		{
-			ID:       5,
-			Name:     "testName",
-			Login:    "testLogin",
-			Password: "testPassword",
-		},
-	}, nil
+func (c *Controller) AllRecords() ([]dbschema.Record, error) {
+	getRecord, err := c.repo.AllRecordsFromDB()
+	if err != nil {
+		c.log.Errorf("Failed to get all records: %w", err)
+	}
+	return getRecord, err
 }
 
 func (c *Controller) GetRecord(id uint64) (*dbschema.Record, error) {
@@ -66,10 +40,12 @@ func (c *Controller) GetRecord(id uint64) (*dbschema.Record, error) {
 func (c *Controller) CreateRecord(records *dbschema.Record) error {
 	return nil
 }
+
 // 200, 204(if no changes?), 404
 func (c *Controller) UpdateRecord(records *dbschema.Record) error {
 	return nil
 }
+
 // 200, 404
 func (c *Controller) DeleteRecord(ids uint64) error {
 	return nil
