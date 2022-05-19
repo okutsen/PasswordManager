6c83b543954017bbe9dc0d5b6f78c4cc9d65c09c
Dmytro Taranov <dtaran@softserveinc.com>
2022-05-19 17:26:53 +0300

    add get all records function and create record function

--- internal/repo/repository.go
+++ internal/repo/repository.go
@@ -1,38 +1,64 @@
 package repo
 
 import (
-	"database/sql"
-	"fmt"
+	"time"
 
-	"github.com/okutsen/PasswordManager/internal/log"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+
 	"github.com/okutsen/PasswordManager/schema/dbschema"
 )
 
 type Repo struct {
-	db *sql.DB
+	db *gorm.DB
 }
 
-func New(cfg *Config, logger log.Logger) (*Repo, error) {
-	logger.Infof("DB is starting")
-	db, err := sql.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
-		cfg.Host, cfg.Port, cfg.Username, cfg.DBName, cfg.Password, cfg.SSLMode))
+func New(cfg *Config) (*Repo, error) {
+	db, err := gorm.Open(postgres.Open(cfg.Address()), &gorm.Config{})
 	if err != nil {
-		return nil, fmt.Errorf("failed to open sql connection: %w", err)
+		return nil, err
 	}
-
-	err = db.Ping()
-	if err != nil {
-		return nil, fmt.Errorf("failed to verify connection to DB: %w", err)
-	}
-
 	return &Repo{
-		db: db}, err
+		db: db,
+	}, err
 }
 
 func (r *Repo) Close() error {
-	return r.db.Close()
+	db, err := r.db.DB()
+	if err != nil {
+		return err
+	}
+	return db.Close()
 }
 
-func AllRecords() ([]dbschema.Record, error) {
-	return []dbschema.Record{}, nil
+func (r *Repo) AllRecordsFromDB() ([]dbschema.Record, error) {
+	var records []dbschema.Record
+	result := r.db.Find(&records).Order("id")
+	err := result.Error
+	if err != nil {
+		return nil, err
+	}
+
+	return records, nil
+}
+
+func (r Repo) CreateRecordInDB() (*dbschema.Record, error) {
+	records := &dbschema.Record{
+		ID:          0,
+		Name:        "John",
+		Login:       "John1823",
+		Password:    "12345",
+		URL:         "john1823.com",
+		Description: "My site",
+		UpdatedBy:   "User",
+		CreatedBy:   "John1823",
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+	}
+	result := r.db.Create(&records)
+	err := result.Error()
+	if err != nil {
+		return nil, err
+	}
+	return records, err
 }
