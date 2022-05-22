298aecd3af4dbdc8fa4c65e2e074fc74a8fc3621
Dmytro Taranov <dtaran@softserveinc.com>
2022-05-23 00:25:47 +0300

    create file to work with DB for records

---
+++ internal/repo/records.go
@@ -0,0 +1,68 @@
+package repo
+
+import (
+	"fmt"
+
+	"github.com/okutsen/PasswordManager/schema/dbschema"
+)
+
+type Records struct {
+	repo *Repo
+}
+
+func NewRecordsRepo(repo *Repo) *Records {
+	return &Records{repo: repo}
+}
+
+func (r *Records) AllRecordsFromDB() ([]dbschema.Record, error) {
+	var records []dbschema.Record
+	result := r.repo.db.Find(&records)
+	err := result.Error
+	if err != nil {
+		return nil, fmt.Errorf("failed to get all records from db: %w", err)
+	}
+
+	return records, err
+}
+
+func (r *Records) CreateRecordInDB(record *dbschema.Record) (*dbschema.Record, error) {
+	result := r.repo.db.Create(record)
+	err := result.Error
+	if err != nil {
+		return nil, fmt.Errorf("failed to create record in db: %w", err)
+	}
+
+	return record, err
+}
+
+func (r *Records) RecordByIDFromDB(id uint64) (*dbschema.Record, error) {
+	var record dbschema.Record
+	result := r.repo.db.First(&record, id)
+	err := result.Error
+	if err != nil {
+		return nil, fmt.Errorf("failed to get record from db: %w", err)
+	}
+
+	return &record, err
+}
+
+func (r *Records) UpdateRecordInDB(record *dbschema.Record) (*dbschema.Record, error) {
+	result := r.repo.db.Model(record).Updates(record)
+	err := result.Error
+	if err != nil {
+		return nil, fmt.Errorf("failed to update record in db: %w", err)
+	}
+
+	return record, err
+}
+
+func (r *Records) DeleteRecordFromDB(id uint64) error {
+	var record dbschema.Record
+	result := r.repo.db.Delete(&record, id)
+	err := result.Error
+	if err != nil {
+		return fmt.Errorf("failed to remove record from db: %w", err)
+	}
+
+	return err
+}
