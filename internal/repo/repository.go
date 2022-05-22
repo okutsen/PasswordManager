bb68cfa19989c1b29de20e139c899f9da8f9f8b9
Dmytro Taranov <dtaran@softserveinc.com>
2022-05-23 00:27:08 +0300

    move methods to separate file

--- internal/repo/repository.go
+++ internal/repo/repository.go
@@ -1,12 +1,8 @@
 package repo
 
 import (
-	"time"
-
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-
-	"github.com/okutsen/PasswordManager/schema/dbschema"
 )
 
 type Repo struct {
@@ -29,36 +25,4 @@ func (r *Repo) Close() error {
 		return err
 	}
 	return db.Close()
-}
-
-func (r *Repo) AllRecordsFromDB() ([]dbschema.Record, error) {
-	var records []dbschema.Record
-	result := r.db.Find(&records).Order("id")
-	err := result.Error
-	if err != nil {
-		return nil, err
-	}
-
-	return records, nil
-}
-
-func (r Repo) CreateRecordInDB() (*dbschema.Record, error) {
-	records := &dbschema.Record{
-		ID:          0,
-		Name:        "John",
-		Login:       "John1823",
-		Password:    "12345",
-		URL:         "john1823.com",
-		Description: "My site",
-		UpdatedBy:   "User",
-		CreatedBy:   "John1823",
-		CreatedAt:   time.Now(),
-		UpdatedAt:   time.Now(),
-	}
-	result := r.db.Create(&records)
-	err := result.Error()
-	if err != nil {
-		return nil, err
-	}
-	return records, err
 }
