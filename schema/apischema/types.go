9cbbda886fb327e45b7774d70d3778453b313738
Dmytro Taranov <dtaran@softserveinc.com>
2022-05-23 00:27:50 +0300

    update strut's fields

--- schema/apischema/types.go
+++ schema/apischema/types.go
@@ -1,17 +1,37 @@
 package apischema
 
+import "time"
+
 const (
+	InvalidIDParam       = "Invalid ID"
 	InvalidJSONMessage   = "Invalid JSON"
 	InternalErrorMessage = "Oops, something went wrong"
 )
 
+type User struct {
+	ID        uint64    `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Login     string    `json:"login"`
+	Password  string    `json:"password"`
+	Phone     string    `json:"phone"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
 // TODO: add validator
 // TODO: add uuid (request id)
 type Record struct {
-	ID       uint64 `json:"id"`
-	Name     string `json:"name"`
-	Login    string `json:"login"`
-	Password string `json:"password"`
+	ID          uint64    `json:"id"`
+	Name        string    `json:"name"`
+	Login       string    `json:"login"`
+	Password    string    `json:"password"`
+	URL         string    `json:"url"`
+	Description string    `json:"description"`
+	UpdatedBy   string    `json:"updated_by"`
+	CreatedBy   string    `json:"created_by"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }
 
 type Error struct {
--- schema/dbschema/types.go
+++ schema/dbschema/types.go
@@ -1,26 +1,29 @@
 package dbschema
 
-import "time"
+import (
+	"time"
+)
 
 type User struct {
-	ID        uint64    `db:"id"`
-	Email     string    `db:"email"`
-	Login     string    `db:"login"`
-	Password  string    `db:"password"`
-	Phone     string    `db:"phone"`
-	CreatedAt time.Time `db:"created_at"`
-	UpdatedAt time.Time `db:"updated_at"`
+	ID        uint64
+	Name      string
+	Email     string
+	Login     string
+	Password  string
+	Phone     string
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
 
 type Record struct {
-	ID          uint64    `db:"id"`
-	Name        string    `db:"name"`
-	Login       string    `db:"login"`
-	Password    string    `db:"password"`
-	URL         string    `db:"url"`
-	Description string    `db:"description"`
-	UpdatedBy   string    `db:"updated_by"`
-	CreatedBy   string    `db:"created_by"`
-	CreatedAt   time.Time `db:"created_at"`
-	UpdatedAt   time.Time `db:"updated_at"`
+	ID          uint64
+	Name        string
+	Login       string
+	Password    string
+	URL         string
+	Description string
+	CreatedBy   string
+	UpdatedBy   string
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
 }
