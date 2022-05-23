6d9ecf5f046059eeb4d3f7c1561612e7d8f58c84
okutsen <okutsen@softserveinc.com>
2022-05-23 19:33:40 +0300

    add local address method to config

--- internal/api/config.go
+++ internal/api/config.go
@@ -8,6 +8,11 @@ type Config struct {
 	Port uint
 }
 
+// TODO: complete address, add Host
 func (c Config) Address() string {
 	return fmt.Sprintf(":%d", c.Port)
+}
+
+func (c Config) LocalAddress() string {
+	return fmt.Sprintf("127.0.0.1:%d", c.Port)
 }
